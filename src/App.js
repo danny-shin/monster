e08fe32d197e762ee81cc94e3ab4154fecb482c4
Daniel Shin <dsshin20@gmail.com>
2022-02-13 17:36:17 +1100

    card, card-list, search-box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
 import {SearchBox} from './components/search-box/search-box.component';
@@ -16,18 +16,27 @@ function App() {
         .then(users => {
           console.log(users);
           setMonsters(users);
+          //setFilterMonsters(users);
         });
     }
     console.log(`useEffect searchField:${searchField}`);
     setFilterMonsters(searchField === '' ? monsters :
-      monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase())
-      || m.email.toLowerCase().includes(searchField.toLowerCase())));
-    console.log(`filterMonsters:\n`, filterMonsters);
-  }, [searchField]); // ComponentDidMount  
+       monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase())
+       || m.email.toLowerCase().includes(searchField.toLowerCase())));
+    // setFilterMonsters(
+    //   monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase())
+    //   || m.email.toLowerCase().includes(searchField.toLowerCase())));
+    console.log(`useEffect filterMonsters:\n`, filterMonsters);
+  }, [searchField, monsters]); // ComponentDidMount  
+
+  const handleChange = e => {
+    setSearchField(e.target.value);
+  }
 
   return (
     <div className="App">
-      <SearchBox placeholder='Search Monsters' handleChange={e =>setSearchField(e.target.value)} />
+      {/* <SearchBox placeholder='Search Monsters' handleChange={e =>setSearchField(e.target.value)} /> */}
+      <SearchBox placeholder='Search Monsters' handleChange={handleChange} />
       <CardList monsters={filterMonsters} />
     </div> 
   );
